9765b4ab343b765fe401a170238a704c8fd81b7c
Joe Bibby <JoeBibbyDev@gmail.com>
2021-05-28 15:43:33 +0100

    comments

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection, useCollectionData } from "react-firebase-hooks/firestore";
 
+// configure project with firebase 
 var firebaseConfig = {
   apiKey: "AIzaSyBV5uiOLLTUMGD-DUkoA5YP7qRUx3udiZo",
   authDomain: "chat-app-9f5e9.firebaseapp.com",
@@ -21,33 +22,33 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
+// create objects in order to access the auth and firestore databse methods
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 
 function App() {
-
+  
   const [user] = useAuthState(auth);
 
   return (
+    //render chatroom if user is signed in, render sign in interface if not
     <div className="App">
       <header>
-
-      </header>
-
+        {user ? <Signout /> : <div></div> }
+      </header>    
       <section>
-        {user ? 
-        <ChatRoom /> : <SignIn />}
+        {user ?
+          <ChatRoom /> : <SignIn />}
       </section>
-      
+
     </div>
   );
 }
 
 
 
-function SignIn(){
+function SignIn() {
 
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -55,73 +56,74 @@ function SignIn(){
   }
 
 
-  return(
+  return (
     <button onClick={signInWithGoogle}>Sign in with Google</button>
   )
 }
 
 
 
-function Signout(){
+function Signout() {
   return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sing out</button>
+    <button onClick={() => auth.signOut()}>Sign out</button>
   )
 }
 
 
 
-function ChatRoom(){
+function ChatRoom() {
 
   const dummy = useRef()
-
+  // get messages in order of creation, useCollectionData listens for updates also, and chatRoom renders again when 
+  // data changes because this is a stateful value. Thus messages show in real time
   const messagesRef = firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt").limit(25);
 
-  const [messages] = useCollectionData(query, {idField: "id"});
-
+  const [messages] = useCollectionData(query, { idField: "id" });
+  // take input from user, store it here to write to database
   const [formValue, setFormValue] = useState("");
 
-  const sendMessage = async(e) => {
+  const sendMessage = async (e) => {
 
     e.preventDefault();
-
+    // get user info to attach to the document
     const { uid, photoURL } = auth.currentUser;
-
+    // write document representing sent message
     await messagesRef.add({
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
       photoURL
     });
-
+    // clear the input box when the user has sent the message
     setFormValue("");
-
-    dummy.current.scrollIntoView({behaviour: "smooth"});
+    // this causes the messages to scroll to the bottom, always showing the most recently sent one
+    dummy.current.scrollIntoView({ behaviour: "smooth" });
   }
 
-  return( 
+  return (
     <div>
       <div>
-        { messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} /> ) }
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
         <div className="dummy" ref={dummy}></div>
       </div>
 
       <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(e) => {setFormValue(e.target.value)}} />
+        <input value={formValue} onChange={(e) => { setFormValue(e.target.value) }} />
         <button type="submit">send</button>
       </form>
     </div>
 
-   
+
   )
 }
 
 
-
+// render the message
 function ChatMessage(props) {
 
   const { text, uid, photoURL } = props.message;
-
+  // check userID attached to message against userID of currently logged in user, display message on the left or right accordingly.
   const messageClass = uid === auth.currentUser.uid ? "sent" : "recieved";
 
   return (
